87b40a741f8cdb7ec27f04a4920fb2b3f696287f
Michał Guminiak <michal.guminiak@tomtom.com>
2014-01-09 21:02:49 +0100

    Fav-tweet-agent initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein with-profile production trampoline run -m fav-tweet-agent.web
---
+++ test/fav_tweet_agent/web_test.clj
@@ -0,0 +1,6 @@
+(ns fav-tweet-agent.web-test
+  (:require [clojure.test :refer :all]
+            [fav-tweet-agent.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
